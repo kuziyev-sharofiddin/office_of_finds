76569269fbba6409f84e2a15daeab9de5fd1c5ed
Sharofiddin <sharofiddin199805@gmail.com>
2023-02-12 23:24:05 +0500

    change host

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=9__$=ir7dff8aq8r*nt20s^oh*#sj!ylajgvv5bwhzpt%p@p9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['188.225.31.249:8000','127.0.0.1:8000' ]
+ALLOWED_HOSTS = ['188.225.31.249','127.0.0.1' ]
 
 
 # Application definition
