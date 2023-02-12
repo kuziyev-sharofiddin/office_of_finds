ba1738cc8892c9c79001b093cee3b67ef32ce15e
Sharofiddin <sharofiddin199805@gmail.com>
2023-02-12 23:46:51 +0500

    change permission

--- finds/views.py
+++ finds/views.py
@@ -5,6 +5,7 @@ from rest_framework import filters
 from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
 from rest_framework import viewsets
+from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.filters import SearchFilter
 
@@ -13,6 +14,7 @@ from rest_framework.filters import SearchFilter
     
 class CategoryViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter]
+    permission_classes = [permissions.IsAuthenticated]
     search_fields = ['name']
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
@@ -20,6 +22,7 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class RegionViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter]
     search_fields = ['name']
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = RegionSerializer
     queryset = Region.objects.all()
 
@@ -27,5 +30,6 @@ class FindsViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter]
     search_fields =['title','types','description', 'region__name','category__name','tag','lost_time','created_at']
     serializer_class = FindsSerializer
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Finds.objects.all()
     
