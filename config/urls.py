3bdffaeebe664811a43c3bcf5be5e4e53a1654ee
Sharofiddin <sharofiddin199805@gmail.com>
2023-02-13 08:31:40 +0500

    changed

--- config/urls.py
+++ config/urls.py
@@ -38,8 +38,7 @@ urlpatterns = [
     path('api/v1/', include('finds.urls')),
     path('api/v1/', include('accounts.urls')),
     path('swagger/', schema_view.with_ui('swagger',
-                                         cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),                                     
+                                         cache_timeout=0), name='schema-swagger-ui'),                                     
     path('redoc/', schema_view.with_ui('redoc',
                                        cache_timeout=0), name='schema-redoc'),
 ]
